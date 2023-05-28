9942866f94bef76bc858705e50419d3b5e8fa637
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-28 18:21:52 -0300

    chore: html changes into confirmEmail template

--- templates/confirmEmail/template_01.ts
+++ templates/confirmEmail/template_01.ts
@@ -13,7 +13,7 @@ function confirm_email_01(title: string, code:number){
     <body style="font-family: Arial, sans-serif; background-color: #f5f5f5; padding: 20px;">
     <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 30px; border-radius: 5px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
     <h1 style="color: #333333; margin-bottom: 20px;">Confirmação de Código</h1>
-    <p style="color: #666666; margin-bottom: 10px;">Olá,</p>
+    <p style="color: #666666; margin-bottom: 10px;">Olá!</p>
     <p style="color: #666666; margin-bottom: 10px;">Seu código de confirmação é: </p>
     <p style="font-size: 24px; font-weight: bold; color: #333333; margin-top: 30px; margin-bottom: 40px;">${code}</p>
     <p style="color: #666666; margin-bottom: 10px;">Utilize este código para confirmar sua conta.</p>
