1b9ed27c0e0e99400c3102a7453e4734c7d8d9a5
yrllanbrandao <yrllanbrandao@outlook.com>
2023-05-28 16:37:29 -0300

    feat: recovery password template

---
+++ templates/confirmEmail/template_01.ts
@@ -0,0 +1,17 @@
+ const confirm_email_01 = () =>{
+   
+
+    // main content
+    return `
+    <div style="max-width: 600px; margin: 0 auto; background-color: #ffffff; padding: 30px; border-radius: 5px; box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);">
+    <h1 style="color: #333333; margin-bottom: 20px;">Confirmação de Código</h1>
+    <p style="color: #666666; margin-bottom: 10px;">Olá,</p>
+    <p style="color: #666666; margin-bottom: 10px;">Seu código de confirmação é:</p>
+    <p style="font-size: 24px; font-weight: bold; color: #333333; margin-top: 30px; margin-bottom: 40px;">123456</p>
+    <p style="color: #666666; margin-bottom: 10px;">Utilize este código para confirmar sua conta.</p>
+  </div>
+    `
+    
+}
+
+export default confirm_email_01;
